55d13acdcc8e3c74a861a551ff0644e04c325825
Andrea Spataro <79209833+andreaspataro@users.noreply.github.com>
2024-04-29 13:47:23 +0200

    ewde

--- page_custom_picks.js
+++ page_custom_picks.js
@@ -143,7 +143,6 @@ function delete_card(obj) {
 }
 
 function edit_card(obj) {
-    console.log(obj.childNodes[0].getAttribute("data-type"));
     let page_anim_out = page_exit(dom_custom_picks_page);
     page_anim_out.onfinish = () => {
         dom_custom_picks_page.style.display = "none";
